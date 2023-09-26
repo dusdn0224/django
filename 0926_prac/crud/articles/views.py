b47c9ff1893ef7320a012be8e9924199d3217f68
이연우 <ckadltmf0224@naver.com>
2023-09-26 17:53:18 +0900

    0926-2

--- 0926_prac/crud/articles/views.py
+++ 0926_prac/crud/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def index(request):
@@ -17,12 +18,62 @@ def detail(request, pk):
     return render(request, 'articles/detail.html', context)
 
 def new(request):
-    return render(request, 'articles/new.html')
+    form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/new.html', context)
 
 def create(request):
+    form = ArticleForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
     title = request.POST.get('title')
     content = request.POST.get('content')
     article = Article(title = title, content = content)
     article.save()
     # return render(request, 'articles/create.html')
+    return redirect('articles:detail', article.pk)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
+
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
     return redirect('articles:detail', article.pk)
